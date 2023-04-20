fe8999b698d84f8ad6491389adec271496a97528
NetherealG <nicolassotov@gmail.com>
2023-04-19 21:55:36 -0400

    first commit

---
+++ clase-dos/hola.py
@@ -0,0 +1,18 @@
+print ("hola mundo!")
+print ("linea nueva")
+
+edad = 24
+sueldo=750000
+nombre = "nicolas"
+isTeacher = False
+
+print(edad)
+print(sueldo)
+print(nombre)
+print(isTeacher)
+
+numero_1 = 1
+numero_2 = 3
+numero_3 = 2
+print((numero_3** numero_2 )/ numero_3)
+
